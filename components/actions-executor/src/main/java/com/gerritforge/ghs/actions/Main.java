78b68904425a432775a17c89096f14cc71c1f944
Marcin Czech <maczech@gmail.com>
2024-03-11 16:09:52 +0100

    Add Nothing Action
    Add skeleton for the actions-executor and a Nothing action.

---
+++ components/actions-executor/src/main/java/com/gerritforge/ghs/actions/Main.java
@@ -0,0 +1,26 @@
+package com.gerritforge.ghs.actions;
+
+import com.google.common.flogger.FluentLogger;
+
+import java.util.concurrent.Callable;
+
+public class Main {
+    private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+
+    public static void main(String[] args) {
+      String action = args[0];
+      String className = Main.class.getPackageName()+"."+action;
+
+        try {
+            Class<Callable<ActionResult>> actionClass = (Class<Callable<ActionResult>>) Class.forName(className);
+            ActionResult result = actionClass.getDeclaredConstructor().newInstance().call();
+            logger.atInfo().log(result.toString());
+        } catch (ClassNotFoundException e) {
+            logger.atSevere().log("Cannot find action class for action name:%s", className);
+        } catch (InstantiationException | IllegalAccessException e) {
+            logger.atSevere().withCause(e).log("Cannot instantiate action class %s, cause %s", className, e.getCause());
+        } catch (Exception e) {
+            logger.atSevere().withCause(e).log("Exception during the action execution. Action class: %s, cause: %s", className, e.getCause());
+        }
+    }
+}
