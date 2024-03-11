f2865ded0a158c390f5cdf1ccdee33608184ca11
Marcin Czech <maczech@gmail.com>
2024-03-11 16:09:52 +0100

    Add Nothing Action
    Add skeleton for the actions-executor and a Nothing action.

---
+++ components/actions-executor/src/main/java/com/gerritforge/ghs/actions/ActionResult.java
@@ -0,0 +1,29 @@
+package com.gerritforge.ghs.actions;
+
+import java.util.Optional;
+
+public class ActionResult {
+  private boolean successful;
+  private Optional<String> message;
+
+  public ActionResult(boolean isSuccessful) {
+    this.successful = isSuccessful;
+  }
+
+  public boolean isSuccessful() {
+    return successful;
+  }
+
+  public Optional<String> getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = Optional.of(message);
+  }
+
+  @Override
+  public String toString() {
+    return "ActionResult{" + "successful=" + successful + ", message=" + message.orElse("") + '}';
+  }
+}
---
+++ components/actions-executor/src/main/java/com/gerritforge/ghs/actions/NothingAction.java
@@ -0,0 +1,12 @@
+package com.gerritforge.ghs.actions;
+
+import java.util.concurrent.Callable;
+
+public class NothingAction implements Callable<ActionResult> {
+  @Override
+  public ActionResult call() throws Exception {
+    ActionResult result = new ActionResult(true);
+    result.setMessage("NoOp action executed successfully");
+    return result;
+  }
+}
